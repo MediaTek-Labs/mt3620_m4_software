16859bd033c2aab141bc105944f9683776d16b65
LawranceLiu <lawrance.liu@mediatek.com>
2020-01-07 23:04:54 -0800

    release_200107

---
+++ MT3620_M4_Sample_Code/FreeRTOS/MT3620_RTApp_FreeRTOS_SPIM/README.md
@@ -0,0 +1,46 @@
+# Sample: MT3620 M4 real-time application - FreeRTos SPIM
+### Description
+This sample demonstrates how to use SPIM on an MT3620 real-time core.
+- ISU1 SPIM interface is used to do the SPI master loopback test.
+- ISU0 UART interface is used to print the output log.
+- Please connect the ISU1_MISO1 to ISU1_MOSI1 for the loopback test.  
+(Note, UART port number in main.c could be changed from **OS_HAL_UART_ISU0** to **OS_HAL_UART_PORT0** to use M4 dedicate UART port.)  
+Please refer to the [MT3620 M4 API Rerference Manual](https://support.mediatek.com/AzureSphere/mt3620/M4_API_Reference_Manual) for the detailed API description.
+
+### Prerequisites
+* **Hardware**
+    * [AVNET MT3620 Starter Kit](https://www.avnet.com/shop/us/products/avnet-engineering-services/aes-ms-mt3620-sk-g-3074457345636825680/)
+    * or [Seeed MT3620 Development Kit](https://aka.ms/azurespheredevkits)
+    * or other hardware that implements the [MT3620 Reference Development Board (RDB)](https://docs.microsoft.com/azure-sphere/hardware/mt3620-reference-board-design) design.
+* **Software**
+    * Refer to [Azure Sphere software installation guide](https://docs.microsoft.com/en-ca/azure-sphere/install/overview).
+    * A terminal emulator (such as Telnet or [PuTTY](https://www.chiark.greenend.org.uk/~sgtatham/putty/) to display the output log).
+
+### How to build and run the sample
+0. Important Note! To reuse the official GCC Cortex-M4F port, the gcc compiler flag should be modified to use FPU instructions. **Please copy the *AzureSphereRTCoreToolchainVFP.cmake* file into the Azure Sphere SDK install folder.** (Default path is *C:\Program Files (x86)\Microsoft Azure Sphere SDK\CMakeFiles*)
+1. Start Visual Studio.  
+2. From **File** menu, select **Open > CMake...** and navigate to the folder that contains this sample.  
+3. Select **CMakeList.txt** and then click **Open**.  
+4. Wait few seconds until Visual Studio finish create the project files.
+5. From **Build** menu, select **Build ALL (Ctrl+Shift+B)**.  
+6. Click **Select Start Item** and then select **GDB Debugger (RTCore)** as following.  
+    ![image](https://raw.githubusercontent.com/MediaTek-Labs/mt3620_m4_software/master/MT3620_M4_Sample_Code/BareMetal/MT3620_RTApp_BareMetal_HelloWorld/pic/select_start_item.jpg)  
+7. Press **F5** to start the application with debugging.  
+
+### Hardware configuration
+* [AVNET MT3620 Starter Kit](https://www.avnet.com/shop/us/products/avnet-engineering-services/aes-ms-mt3620-sk-g-3074457345636825680/)
+    * Connect ISSU1_MISO1 to ISU1_MISO0 for SPI loopback:
+        ![image](https://raw.githubusercontent.com/MediaTek-Labs/mt3620_m4_software/master/MT3620_M4_Sample_Code/BareMetal/MT3620_RTApp_BareMetal_HelloWorld/pic/avnet_spim_loopback.png)  
+        ![image](https://raw.githubusercontent.com/MediaTek-Labs/mt3620_m4_software/master/MT3620_M4_Sample_Code/BareMetal/MT3620_RTApp_BareMetal_HelloWorld/pic/avnet_spim_loopback_connect.png)  
+    * Connect PC UART Rx to AVNET MT3620 Starter Kit Click #1 TX (ISU0_UART_TX):
+        ![image](https://raw.githubusercontent.com/MediaTek-Labs/mt3620_m4_software/master/MT3620_M4_Sample_Code/BareMetal/MT3620_RTApp_BareMetal_HelloWorld/pic/avnet.jpg)  
+        ![image](https://raw.githubusercontent.com/MediaTek-Labs/mt3620_m4_software/master/MT3620_M4_Sample_Code/BareMetal/MT3620_RTApp_BareMetal_HelloWorld/pic/avnet_connect.jpg)  
+* [Seeed MT3620 Development Kit](https://aka.ms/azurespheredevkits)
+    * Connect ISSU1_MISO1 to ISU1_MISO0 for SPI loopback:
+        ![image](https://raw.githubusercontent.com/MediaTek-Labs/mt3620_m4_software/master/MT3620_M4_Sample_Code/BareMetal/MT3620_RTApp_BareMetal_HelloWorld/pic/seeed_spim_loopback.png)  
+        ![image](https://raw.githubusercontent.com/MediaTek-Labs/mt3620_m4_software/master/MT3620_M4_Sample_Code/BareMetal/MT3620_RTApp_BareMetal_HelloWorld/pic/seeed_spim_loopback_connect.png)  
+    * Connect PC UART Rx to Seeed MT3620 Development Kit GPIO 26 / TXD0  (ISU0_UART_TX):
+        ![image](https://raw.githubusercontent.com/MediaTek-Labs/mt3620_m4_software/master/MT3620_M4_Sample_Code/BareMetal/MT3620_RTApp_BareMetal_HelloWorld/pic/seeed.jpg)  
+        ![image](https://raw.githubusercontent.com/MediaTek-Labs/mt3620_m4_software/master/MT3620_M4_Sample_Code/BareMetal/MT3620_RTApp_BareMetal_HelloWorld/pic/seeed_connect.jpg)  
+	* M4 dedicate UART port is also available on Seeed MT3620 Development Kit:
+        ![image](https://raw.githubusercontent.com/MediaTek-Labs/mt3620_m4_software/master/MT3620_M4_Sample_Code/BareMetal/MT3620_RTApp_BareMetal_HelloWorld/pic/seeed_m4_dedicate_uart.jpg)  
---
+++ MT3620_M4_Sample_Code/FreeRTOS/MT3620_RTApp_FreeRTOS_SPIM/main.c
@@ -0,0 +1,239 @@
+/*
+ * (C) 2005-2019 MediaTek Inc. All rights reserved.
+ *
+ * Copyright Statement:
+ *
+ * This MT3620 driver software/firmware and related documentation
+ * ("MediaTek Software") are protected under relevant copyright laws.
+ * The information contained herein is confidential and proprietary to
+ * MediaTek Inc. ("MediaTek"). You may only use, reproduce, modify, or
+ * distribute (as applicable) MediaTek Software if you have agreed to and been
+ * bound by this Statement and the applicable license agreement with MediaTek
+ * ("License Agreement") and been granted explicit permission to do so within
+ * the License Agreement ("Permitted User"). If you are not a Permitted User,
+ * please cease any access or use of MediaTek Software immediately.
+ *
+ * BY OPENING THIS FILE, RECEIVER HEREBY UNEQUIVOCALLY ACKNOWLEDGES AND AGREES
+ * THAT MEDIATEK SOFTWARE RECEIVED FROM MEDIATEK AND/OR ITS REPRESENTATIVES ARE
+ * PROVIDED TO RECEIVER ON AN "AS-IS" BASIS ONLY. MEDIATEK EXPRESSLY DISCLAIMS
+ * ANY AND ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR
+ * NONINFRINGEMENT. NEITHER DOES MEDIATEK PROVIDE ANY WARRANTY WHATSOEVER WITH
+ * RESPECT TO THE SOFTWARE OF ANY THIRD PARTY WHICH MAY BE USED BY,
+ * INCORPORATED IN, OR SUPPLIED WITH MEDIATEK SOFTWARE, AND RECEIVER AGREES TO
+ * LOOK ONLY TO SUCH THIRD PARTY FOR ANY WARRANTY CLAIM RELATING THERETO.
+ * RECEIVER EXPRESSLY ACKNOWLEDGES THAT IT IS RECEIVER'S SOLE RESPONSIBILITY TO
+ * OBTAIN FROM ANY THIRD PARTY ALL PROPER LICENSES CONTAINED IN MEDIATEK
+ * SOFTWARE. MEDIATEK SHALL ALSO NOT BE RESPONSIBLE FOR ANY MEDIATEK SOFTWARE
+ * RELEASES MADE TO RECEIVER'S SPECIFICATION OR TO CONFORM TO A PARTICULAR
+ * STANDARD OR OPEN FORUM. RECEIVER'S SOLE AND EXCLUSIVE REMEDY AND MEDIATEK'S
+ * ENTIRE AND CUMULATIVE LIABILITY WITH RESPECT TO MEDIATEK SOFTWARE RELEASED
+ * HEREUNDER WILL BE ANY SOFTWARE LICENSE FEES OR SERVICE CHARGE PAID BY
+ * RECEIVER TO MEDIATEK DURING THE PRECEDING TWELVE (12) MONTHS FOR SUCH
+ * MEDIATEK SOFTWARE AT ISSUE.
+ */
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "printf.h"
+#include "mt3620.h"
+
+#include "os_hal_uart.h"
+#include "os_hal_spim.h"
+
+/******************************************************************************/
+/* Configurations */
+/******************************************************************************/
+static const uint8_t uart_port_num = OS_HAL_UART_ISU0;
+
+static uint8_t spi_master_port_num = OS_HAL_SPIM_ISU1;
+static uint32_t spi_master_speed = 100; 				// 100KHz
+
+#define SPIM_CLOCK_POLARITY SPI_CPOL_0
+#define SPIM_CLOCK_PHASE SPI_CPHA_0
+#define SPIM_RX_MLSB SPI_MSB
+#define SPIM_TX_MSLB SPI_MSB
+#define SPIM_FULL_DUPLEX_MIN_LEN 2
+#define SPIM_FULL_DUPLEX_MAX_LEN 17
+
+#define APP_STACK_SIZE_BYTES		(1024 / 4)
+
+/******************************************************************************/
+/* Applicaiton Hooks */
+/******************************************************************************/
+// Hook for "stack over flow".
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char* pcTaskName)
+{
+	printf("%s: %s\r\n", __func__, pcTaskName);
+}
+
+// Hook for "memory allocation failed".
+void vApplicationMallocFailedHook(void)
+{
+	printf("%s\r\n", __func__);
+}
+// Hook for "printf".
+void _putchar(char character)
+{
+	mtk_os_hal_uart_put_char(uart_port_num, character);
+}
+
+/******************************************************************************/
+/* Global Variables */
+/******************************************************************************/
+static struct mtk_spi_config spi_default_config = {
+	.cpol = SPIM_CLOCK_POLARITY,
+	.cpha = SPIM_CLOCK_PHASE,
+	.rx_mlsb = SPIM_RX_MLSB,
+	.tx_mlsb = SPIM_TX_MSLB,
+	.slave_sel = SPI_SELECT_DEVICE_0,
+};
+static uint8_t spim_tx_buf[SPIM_FULL_DUPLEX_MAX_LEN];
+static uint8_t spim_rx_buf[SPIM_FULL_DUPLEX_MAX_LEN];
+static volatile int g_async_done_flag;
+
+/******************************************************************************/
+/* Functions */
+/******************************************************************************/
+static int spi_xfer_complete(void *context)
+{
+	g_async_done_flag = 1;
+	return 0;
+}
+
+static int spi_loopback_check(struct mtk_spi_transfer *trans)
+{
+	int i, value, err = 0;
+
+	/* i = 0 is opcode */
+	for (i = 1; i < trans->len; i++) {
+		value = *((u8 *) trans->tx_buf + i);
+		if (value != *((char *) trans->rx_buf + i))
+			err++;
+	}
+
+	if (err) {
+		return -1;
+	} else {
+		return 0;
+	}
+}
+
+static int spi_async_transfer_test(int bus_num, int use_dma, int length, int khz)
+{
+	int ret = 0, i;
+	struct mtk_spi_transfer xfer;
+
+	memset(&xfer, 0, sizeof(xfer));
+
+	xfer.tx_buf = spim_tx_buf;
+	xfer.rx_buf = spim_rx_buf;
+	xfer.use_dma = use_dma;
+	xfer.speed_khz = khz;
+	xfer.len = length;
+
+	for (i = 1; i < xfer.len; i++)
+		*((char *)xfer.tx_buf + i) = 0x11 + i;
+
+	for (i = 1; i < xfer.len; i++)
+		*((char *)xfer.rx_buf + i) = 0xcc;
+
+	g_async_done_flag = 0;
+	ret = mtk_os_hal_spim_async_transfer((spim_num) bus_num,
+			 &spi_default_config, &xfer, spi_xfer_complete, &xfer);
+	if (ret) {
+		printf("mtk_os_hal_spim_async_transfer fail\n");
+		return ret;
+	}
+
+	while (g_async_done_flag == 0)
+		vTaskDelay(pdMS_TO_TICKS(10));
+
+	ret = spi_loopback_check(&xfer);
+	return ret;
+}
+
+static int spi_transfer_test(int bus_num, int use_dma, int length, int khz)
+{
+	int ret = 0, i;
+	struct mtk_spi_transfer xfer;
+
+	memset(&xfer, 0, sizeof(xfer));
+
+	xfer.tx_buf = spim_tx_buf;
+	xfer.rx_buf = spim_rx_buf;
+	xfer.use_dma = use_dma;
+	xfer.speed_khz = khz;
+	xfer.len = length;
+
+	for (i = 1; i < xfer.len; i++)
+		*((char *)xfer.tx_buf + i) = 0x11 + i;
+
+	for (i = 1; i < xfer.len; i++)
+		*((char *)xfer.rx_buf + i) = 0xcc;
+
+	ret = mtk_os_hal_spim_transfer((spim_num) bus_num,
+				 &spi_default_config, &xfer);
+	if (ret) {
+		printf("mtk_os_hal_spim_transfer failed\r\n");
+		return ret;
+	}
+
+	ret = spi_loopback_check(&xfer);
+	return ret;
+}
+
+static void spim_task(void* pParameters)
+{
+	uint32_t counter=0;
+	uint8_t i=0;
+	uint8_t err=0;
+
+	printf("SPI master sync/async transfer test with legth %d ~ %d. \r\n", 
+			SPIM_FULL_DUPLEX_MIN_LEN, SPIM_FULL_DUPLEX_MAX_LEN);
+	while (1) {
+		err=0;
+		vTaskDelay(pdMS_TO_TICKS(1000));
+
+		for (i = SPIM_FULL_DUPLEX_MIN_LEN; i <= SPIM_FULL_DUPLEX_MAX_LEN; i++) {
+			// Synchronous loopback test.
+			if (spi_transfer_test(spi_master_port_num, 0, i, spi_master_speed)) {
+				printf("spi_transfer_test error! (length=%d)\r\n", i);
+				err++;
+			}
+			// Asynchronous loopback test.
+			if (spi_async_transfer_test(spi_master_port_num, 0, i, spi_master_speed)) {
+				printf("spi_async_transfer_test error! (length=%d)\r\n", i);
+				err++;
+			}
+		}
+
+		if (err) {
+			printf("Test iteration[%ld] result: FAILED\r\n", counter++);
+		} else {
+			printf("Test iteration[%ld] result: PASSED\r\n", counter++);
+		}
+	}
+}
+
+_Noreturn void RTCoreMain(void)
+{
+	// Setup Vector Table
+	NVIC_SetupVectorTable();
+
+	// Init UART
+	mtk_os_hal_uart_ctlr_init(uart_port_num);
+	printf("FreeRTOS SPIM demo\r\n");
+
+	// Init SPIM
+	mtk_os_hal_spim_ctlr_init(spi_master_port_num);
+
+	// Create SPIM Task
+	xTaskCreate(spim_task, "SPIM Task", APP_STACK_SIZE_BYTES, NULL, 4, NULL);
+	vTaskStartScheduler();
+
+	for (;;) {
+		__asm__("wfi");
+	}
+}
+
